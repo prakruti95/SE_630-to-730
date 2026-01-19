6da1b08d8913de93c02d4cef652370ad2559f314
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2026-01-19 19:09:34 +0530

    commit

---
+++ Module1/19-1/main (3).c
@@ -0,0 +1,13 @@
+//Area of Circle
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        float pi = 3.14;
+        int r = 5;
+        
+        float ans = pi*r*r;
+        printf("Area of Circle is : %f",ans);
+    
+    getch();
+}
---
+++ Module1/19-1/main (4).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float value = 0.5;
+    int a = 6;
+    int b = 5;
+    
+    float ans = value*a*b;
+    printf("Ans is : %f ",ans);
+    
+
+  getch();
+}
